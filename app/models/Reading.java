62d81a2e65039be9787d6a7b9b73361371b7c837
Darragh Conneely <darraghc23@gmail.com>
2022-05-22 21:10:46 +0100

    added max and min for temperature, pressure, and wind speed in: - addreading.html partial - latestweather.html partial - Reading.java model

--- app/models/Reading.java
+++ app/models/Reading.java
@@ -13,9 +13,13 @@ public class Reading extends Model
     public double windSpeed;
     public int windDirection;
     public long pressure;
-    private HashMap<Integer, String> weatherCodeMap;
-
-
+    
+    public double maxTemperature;
+    public double minTemperature;
+    public long maxPressure;
+    public long minPressure;
+    public double maxWindSpeed;
+    public double minWindSpeed;
 
     public Reading(int code, double temperature, double windSpeed, long pressure, int windDirection)
     {
@@ -25,4 +29,22 @@ public class Reading extends Model
         this.pressure = pressure;
         this.windDirection = windDirection;
     }
+
+
+    public Reading(int code, double temperature, double windSpeed, long pressure, int windDirection, double maxTemperature, double minTemperature, long maxPressure, long minPressure, double maxWindSpeed, double minWindSpeed)
+    {
+        this.code = code;
+        this.temperature = temperature;
+        this.windSpeed = windSpeed;
+        this.pressure = pressure;
+        this.windDirection = windDirection;
+        this.maxTemperature = maxTemperature;
+        this.minTemperature = minTemperature;
+        this.maxPressure = maxPressure;
+        this.minPressure = minPressure;
+        this.maxWindSpeed = maxWindSpeed;
+        this.minWindSpeed = minWindSpeed;
+    }
+   
+    
 }
